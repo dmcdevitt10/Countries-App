ff8ca19c837eec0a61d66dd3301900f9d576503d
Daniel McDevitt <danielmcdevitt10@gmail.com>
2023-05-09 14:13:54 -0600

    countries are now being saved to global state and displayed in the browser

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,15 @@
 import axios from "axios";
 import React, { useState } from "react";
 import { BsFillFlagFill } from "react-icons/bs";
+import { useDispatch } from "react-redux";
+
+import {deletePotentialCountries, setPotentialCountries} from '../redux/slices/potentialCountriesSlice'
 
 const Header = () => {
     const [input, setInput] = useState();
+
+    const dispatch = useDispatch()
+
     return (
         <div className="header">
             <div className="home">
@@ -24,6 +30,8 @@ const Header = () => {
                         axios
                             .get(`https://restcountries.com/v3.1/name/${input}`)
                             .then((res) => {
+                                dispatch(deletePotentialCountries())
+                                dispatch(setPotentialCountries(res.data))
                                 console.log(res.data);
                             })
                             .catch((err) => {
--- src/Components/OptionDisplay.js
+++ src/Components/OptionDisplay.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectPotentials } from "../redux/slices/potentialCountriesSlice";
 
 const OptionDisplay = () => {
-    return <div className="stack">OptionDisplay</div>;
+    const currentPontentials = useSelector(selectPotentials)
+    console.log(currentPontentials)
+
+    return <div className="stack">{currentPontentials?.map((country) => {
+        return <h2>{country.name.common}</h2>
+    })}</div>;
 };
 
 export default OptionDisplay;
