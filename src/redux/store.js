478b8569efddabb7d6099acd56b6c5ce3a19f39d
Daniel McDevitt <danielmcdevitt10@gmail.com>
2023-05-10 15:31:05 -0600

    now have the countries console.logged so they are ready to be displayed

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import potentialCountriesReducer from "../redux/slices/potentialCountriesSlice";
 
+import displayedCountryReducer from "./slices/displayCountrySlice";
+
 export default configureStore({
-  reducer: { potentialCountries: potentialCountriesReducer },
+  reducer: {
+    potentialCountries: potentialCountriesReducer,
+    displayedCountry: displayedCountryReducer,
+  },
 });
