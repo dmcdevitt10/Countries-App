40cac1c993b360b812feb6eb08c20da253408b1f
Daniel McDevitt <danielmcdevitt10@gmail.com>
2023-05-12 14:03:53 -0600

    flag and coat of arms is now displayed on the symbols page

--- src/Components/Overview.js
+++ src/Components/Overview.js
@@ -7,54 +7,54 @@ const Overview = () => {
 
   return (
     <div className="stack">
-            <h1>{currentDisplay.name.official}</h1>
-            <h2>"{currentDisplay.name.common}"</h2>
+      <h1>{currentDisplay.name.official}</h1>
+      <h2>"{currentDisplay.name.common}"</h2>
 
-            <table className="overview-table">
-                <tr>
-                    <td>Borders: </td>
-                    <td>
-                        {currentDisplay.borders
-                            ? currentDisplay.borders.map((e, i, arr) => {
-                                  if (i + 1 === arr.length) {
-                                      return `${e}`;
-                                  } else {
-                                      return `${e}, `;
-                                  }
-                              })
-                            : "N/A"}
-                    </td>
-                </tr>
-                <tr>
-                    <td>Capitol: </td>
-                    {currentDisplay.capital.map((e) => (
-                        <td>{e}</td>
-                    ))}
-                </tr>
-                <tr>
-                    <td>Population: </td>
-                    <td>{currentDisplay.population}</td>
-                </tr>
-                <tr>
-                    <td>Continents: </td>
-                    {currentDisplay.continents.map((e) => (
-                        <td>{e}</td>
-                    ))}
-                </tr>
-                <tr>
-                    <td>Independent: </td>
-                    <td>{currentDisplay.independent ? "true" : "false"}</td>
-                </tr>
-                <tr>
-                    <td>Landlocked: </td>
-                    <td>{currentDisplay.landlocked ? "true" : "false"}</td>
-                </tr>
-                <tr>
-                    <td>Member of UN: </td>
-                    <td>{currentDisplay.unMember ? "true" : "false"}</td>
-                </tr>
-            </table>
-        </div>
+      <table className="overview-table">
+        <tr>
+          <td>Borders: </td>
+          <td>
+            {currentDisplay.borders
+              ? currentDisplay.borders.map((e, i, arr) => {
+                  if (i + 1 === arr.length) {
+                    return `${e}`;
+                  } else {
+                    return `${e}, `;
+                  }
+                })
+              : "N/A"}
+          </td>
+        </tr>
+        <tr>
+          <td>Capitol: </td>
+          {currentDisplay.capital.map((e) => (
+            <td>{e}</td>
+          ))}
+        </tr>
+        <tr>
+          <td>Population: </td>
+          <td>{currentDisplay.population}</td>
+        </tr>
+        <tr>
+          <td>Continents: </td>
+          {currentDisplay.continents.map((e) => (
+            <td>{e}</td>
+          ))}
+        </tr>
+        <tr>
+          <td>Independent: </td>
+          <td>{currentDisplay.independent ? "true" : "false"}</td>
+        </tr>
+        <tr>
+          <td>Landlocked: </td>
+          <td>{currentDisplay.landlocked ? "true" : "false"}</td>
+        </tr>
+        <tr>
+          <td>Member of UN: </td>
+          <td>{currentDisplay.unMember ? "true" : "false"}</td>
+        </tr>
+      </table>
+    </div>
   );
 };
 
